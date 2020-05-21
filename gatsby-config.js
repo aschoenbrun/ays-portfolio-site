00ca10a35245a90c55aafc25d75d1b55c31021a6
effectivemedia <avi@youreffectivemedia.com>
2020-05-21 12:58:25 -0400

    Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,6 +45,23 @@ module.exports = {
         classes: false,
       },
     },
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: process.env.GATSBY_GOOGLE_ANALYTICS_TRACKING_ID,
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Defers execution of google analytics script after page load
+        defer: false,
+      },
+    },
     "gatsby-plugin-sharp",
     // {
     //   resolve: "gatsby-plugin-manifest",
