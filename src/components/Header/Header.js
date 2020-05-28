b9ee703a5ff5f9f594bfb72c5b9f3a266f76b7ec
effectivemedia <avi@youreffectivemedia.com>
2020-05-28 11:42:13 -0400

    Applied HEaderoom

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components/macro";
+import Headroom from "react-headroom";
 import HeaderBartop from "./HeaderBarTop";
 import HeaderBarBottom from "./HeaderBarBottom";
 import HeaderNav from "./HeaderNav";
@@ -9,14 +10,33 @@ const HeaderStyles = styled.header`
   padding: 0;
   position: relative;
   z-index: 3000;
+  .headroom-wrapper {
+    margin-bottom: 80px;
+    @media screen and (min-width: 960px) {
+      margin-bottom: 50px;
+    }
+    .headroom--unpinned,
+    .headroom--scrolled {
+      @media screen and (max-width: 960px) {
+        position: static !important;
+      }
+    }
+    .headroom--scrolled {
+      @media screen and (min-width: 760px) {
+        width: 100%;
+      }
+    }
+  }
 `;
 
 const Header = () => {
   return (
     <HeaderStyles>
-      <HeaderBartop />
-      <HeaderBarBottom />
-      <HeaderNav />
+      <Headroom pinStart={500}>
+        <HeaderBartop />
+        <HeaderBarBottom />
+        <HeaderNav />
+      </Headroom>
     </HeaderStyles>
   );
 };
