bf00524c92e8bd2691fc4bf3045087eb45f7482d
effectivemedia <avi@youreffectivemedia.com>
2020-06-23 11:49:42 -0400

    Reactivated prompt

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,10 @@
 // Prompt user to update page
-// export const onServiceWorkerUpdateReady = () => {
-//   const answer = window.confirm(
-//     `This application has been updated. ` +
-//       `Reload to display the latest version?`
-//   );
-//   if (answer === true) {
-//     window.location.reload();
-//   }
-// };
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `This application has been updated. ` +
+      `Reload to display the latest version?`
+  );
+  if (answer === true) {
+    window.location.reload();
+  }
+};
