140b4bf35e9c090775e6196111fce2f880e10f15
effectivemedia <avi@youreffectivemedia.com>
2020-06-22 22:53:23 -0400

    Deploy IYH

--- gatsby-config.js
+++ gatsby-config.js
@@ -81,7 +81,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "UA-170386036-1",
+        trackingId: process.env.GATSBY_GOOGLE_ANALYTICS_TRACKING_ID,
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: false,
         // Defers execution of google analytics script after page load
